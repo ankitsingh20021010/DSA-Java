23cd8ab309c2700de71d8531e37a2c4a1ced170b
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2025-12-20 00:24:44 +0530

    sum array elements

---
+++ Arrays/sum.java
@@ -0,0 +1,16 @@
+//sum of array element using java
+package array;
+
+public class sum {
+    public static void main(String[]args){
+        int[]arr={10,20,30,40,50};
+        int m=0;
+        for(int i=0; i<5; i++){
+            int n=arr[i];
+            m=m+n;
+
+        }
+        System.out.println(m);
+    }
+    
+}
