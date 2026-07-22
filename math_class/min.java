eab982d4e6221387ef8683c54f26bad174478ed8
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 14:44:47 +0530

    Add min.java to find minimum of two numbers

---
+++ math_class/min.java
@@ -0,0 +1,11 @@
+public class min{
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int max = Math.min(a, b);
+
+        System.out.println("Maximum = " + max);
+    }
+}
