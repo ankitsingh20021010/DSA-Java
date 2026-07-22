967223dd2a1c7764a8b351e1ddb64272049c233d
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 14:57:05 +0530

    Add comment about comparison operator in IfDemo
    Added comment explaining the comparison operator.

---
+++ java_basic/IfDemo.java
@@ -0,0 +1,9 @@
+public class IfDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        //greater then or equalto use to comapir value it is a also operator
+        if (age >= 18) {
+            System.out.println("Eligible to Vote");
+        }
+    }
+}
