1428c8908cf878af25dc26ecf7515bc4cd129f29
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 15:00:05 +0530

    Add comment for largest of two numbers functionality
    Added comment to indicate functionality of the class.

---
+++ java_basic/Maximum.java
@@ -0,0 +1,13 @@
+public class Maximum {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 25;
+
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+    }
+}
+//Largest of Two Numbers
