131d123d3237b80e25a6ab35c881a182456dac21
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 15:04:23 +0530

    Add for loop example in for.java
    Implement a for loop to print numbers from 1 to 10.

---
+++ java_basic/loop/for.java
@@ -0,0 +1,10 @@
+public class for {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
+//output 1,2,3,4,5,6,7,8,9,10 for every next line(vertical)
