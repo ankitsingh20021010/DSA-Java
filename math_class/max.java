44f4eead33fd462c5314bf476dff506bbc57062a
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 14:44:09 +0530

    Rename class from 'min' to 'max'

---
+++ math_class/max.java
@@ -0,0 +1,11 @@
+public class max{
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int max = Math.max(a, b);
+
+        System.out.println("Maximum = " + max);
+    }
+}
