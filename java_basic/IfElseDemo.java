cc793267c8659076ea52c02c5ffb5a4db377be24
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 14:58:20 +0530

    Fix formatting and add comment in IfElseDemo.java

---
+++ java_basic/IfElseDemo.java
@@ -0,0 +1,13 @@
+public class IfElseDemo {
+    public static void main(String[] args) {
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("Eligible");
+        } else {
+            System.out.println("Not Eligible");
+        }
+    }
+}
+
+//if=agar //else=warna
