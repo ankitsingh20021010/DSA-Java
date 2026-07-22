0cf55e7aae66fb96cba42bb7b7f262f888ef3760
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-22 14:55:53 +0530

    Enhance Sum.java with alternative print methods
    Added alternative methods for printing the sum.

---
+++ java_basic/Sum.java
@@ -0,0 +1,12 @@
+public class Sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Sum = " + (a + b));
+    }
+}
+
+//another way to print 
+//sum= a+b;
+//sout(sum);
