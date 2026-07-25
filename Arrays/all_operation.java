9926aafd07f80fe227cae4191f6cdc832e50c546
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-25 17:39:44 +0530

    Refactor array operations and add new methods
    Updated methods for better clarity and functionality. Added deleteAtEnd and addAtend methods for array operations.

--- Arrays/all_operation.java
+++ Arrays/all_operation.java
@@ -1,14 +1,14 @@
 //try to all array operations in data structure
 import java.util.*;
 public class all_operation{
-    static void printarr(int[] arr){
+  static void printarr(int[] arr){
         System.out.print("Array is: ");
         for(int i=0; i<arr.length; i++){ 
             System.out.print(arr[i] + " ");
         }
         System.out.println(" ");
     }
-//cash2 at possition
+//cash2 enter element at possition
       static void atpossition(int []arr, Scanner sc){
         System.out.print("Enter possition (1 to" + arr.length + ":)");
         int pos=sc.nextInt();
@@ -25,12 +25,12 @@ public class all_operation{
  static int[] insert_start(int[] arr, Scanner sc){
     System.out.print("enter number:-");
     int num=sc.nextInt();
-    int [] newArr=new int[arr.length + 1];
+    int [] newArr=new int[arr.length + 1]; //static array
     newArr[0]=num;
     for(int i=0; i<arr.length; i++){
          newArr[i+1]=arr[i];
     }
-    return newArr;
+    return newArr; //after ruturn diynamic array
  }
  //delete element 
  static int[] delete_arr(int[] arr){
@@ -39,7 +39,6 @@ public class all_operation{
         newarr[i]= arr[i+1];
     }
     return newarr;
-
  }
  //search element
  static void search_arr(int[] arr,Scanner sc){
@@ -48,31 +47,52 @@ public class all_operation{
     int flag=0;
     for(int i=0; i<arr.length; i++){
         if(arr[i]==element){
-            flag++;
+            flag++; 
         }
     }
-    if(flag==1){
-        System.out.println();
+    if(flag==1){ 
+        System.out.println();//output look diffrent easy to ready or underustand 
         System.out.println();
         System.out.println("Yes element "+ element+" is exist");
         System.out.println();
         System.out.println();
-
     }
     else{
         System.out.println();
         System.out.println("No elemet "+ element+" is not exist");
     }
  }
+  //case:6 delete at end possition 
+ static int[] deleteAtEnd(int[] arr){
+    int[] newarr=new int[arr.length-1];
+    for(int i=0; i<arr.length-1; i++){
+        newarr[i]=arr[i];
+    }
+    return arr=newarr;
+ }
+ //case:7 add at end possition
+ static int[] addAtend(int[] arr, Scanner sc){
+    System.out.print("enter last element :");
+    int num=sc.nextInt();
+    int [] newarr=new int[arr.length+1];
+    for(int i=0; i<arr.length; i++){
+        newarr[i]=arr[i];
+    }
+    newarr[arr.length]=num;
+    return newarr;
+ }
+ //main funtion start hear
     public static void main(String[] args) {
      int[] arr={1,2,3,4,5,6,7,8};
      int i;
      do{ 
-        System.out.println("1 for print array");
-        System.out.println("2 for enter data at possition");
-        System.out.println("3 for insert start possition");
+        System.out.println("1 for Print array");
+        System.out.println("2 for Enter element at possition");
+        System.out.println("3 for Insert start possition");
         System.out.println("4 for Delete at start possition");
         System.out.println("5 for Search element in array");
+        System.out.println("6 for Delete element last possition");
+        System.out.println("7 for insert element last possition");
         System.out.print("enter number: ");
         Scanner sc=new Scanner(System.in);
         i=sc.nextInt();
@@ -90,14 +110,23 @@ public class all_operation{
             case 4:
             arr=delete_arr(arr);
             printarr(arr);
-            break;
+                 break;
             case 5:
             search_arr(arr,sc);
-            default: 
+            break;
+            case 6:
+            arr=deleteAtEnd(arr);
+            printarr(arr);
+            break;
+            case 7:
+            arr=addAtend(arr,sc);
+            printarr(arr);
+            break;
+            default:
+                System.out.println("----Please enter valid number ----");
                 break;
-        }
-        
-     } while(i >0);
+        }   
+     } while(i>0);
 
     }
 }
