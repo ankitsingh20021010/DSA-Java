502d0ca2fcfce23e4fe04e86b0d1972102c6ae82
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-07-31 20:51:52 +0530

    Add basic Singly Linked List implementation
    Created the initial implementation of a Singly Linked List in Java.
    
    Features added:
    - Created Node class with data and next reference.
    - Added head and tail references.
    - Implemented addFirst() method to insert nodes at the beginning.
    - Implemented display() method to traverse and print the list.
    - Implemented range() method to count the total number of nodes.
    - Established the basic linked list foundation for future operations such as addLast, delete, search, and reverse.

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -1,1 +1,67 @@
 
+public class SinglyLinkedList {
+    Node head=null;
+    Node tail=null;
+   static class Node{
+    int data;
+    Node next;
+     Node(int data){
+        this.data=data;
+     }
+   }
+   int range(){
+    int count=0;
+    Node temp=head;
+    if(head==null){
+     return 0;
+    }
+    while(temp!=null){
+        count++;
+        temp=temp.next;
+    }
+     return count;
+   }
+   void display(){
+      Node temp=head;
+      if(head==null){
+        System.out.println("List empty");
+        return;
+      }
+      while(temp!=null){
+         System.out.print(temp.data + " --->");
+         temp=temp.next;
+      }
+   }
+ void addFirst(int data){
+    Node newNode = new Node(data);
+   
+    if(head==null){
+        head=newNode;
+        tail = newNode;
+        return;
+        
+    }
+    else{
+       
+        newNode.next=head;
+        head=newNode;
+    }
+ }
+   public static void main(String[] args) {
+    SinglyLinkedList ll=new SinglyLinkedList();
+    Node list1=new Node(5);
+    ll.head=list1;
+    ll.tail=list1;
+    ll.addFirst(10);
+    ll.addFirst(15);
+    ll.addFirst(20);
+    int size = ll.range();
+    ll.display();
+
+    System.out.println(" and their size is "+size);
+    // Node list2=ll.new Node(6);
+    // Node list3=ll.new Node(7);
+    
+   }
+    
+}
