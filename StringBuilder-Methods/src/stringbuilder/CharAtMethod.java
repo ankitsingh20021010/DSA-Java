95d033604dcddf6d924b527e8c3ee9fae25f10da
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-01 14:25:01 +0530

    Implement CharAtMethod to demonstrate charAt usage
    Added a CharAtMethod class demonstrating the use of the charAt method in StringBuilder.

--- StringBuilder-Methods/src/stringbuilder/CharAtMethod.java
+++ StringBuilder-Methods/src/stringbuilder/CharAtMethod.java
@@ -1,1 +1,19 @@
 
+//CharAt method use to return a single char use to index number and in java String index start at 0to n number
+
+class  CharAtMethod{
+    
+    public static void main(String[] args) {
+        StringBuilder sd=new StringBuilder("ANKIT");
+        //A N K I T
+        //0 1 2 3 4
+        char ch=sd.charAt(3);
+      char n=sd.charAt(4);
+        System.out.println(ch);
+        System.out.println(n);
+    }
+}
+
+//output 
+//I
+//T
