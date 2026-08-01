5721ccaa6534bb421f15220cc421185b24c5bbc8
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-01 14:33:14 +0530

    Add example of StringBuilder insert method
    Demonstrate the use of StringBuilder's insert method to add a substring at a specified index.

--- StringBuilder-Methods/src/stringbuilder/InsertMethod.java
+++ StringBuilder-Methods/src/stringbuilder/InsertMethod.java
@@ -1,1 +1,19 @@
 
+//insert
+//sb.insert(index,"value")
+//sb.insert(3,"Hyy");
+class InsertMethod{
+    
+    public static void main(String[] args) {
+        StringBuilder sd=new StringBuilder("ANKIT");
+        //A N K I T
+        //0 1 2 3 4
+        sd.insert(3,"hyy");
+        //N K I 
+        System.out.println(sd);
+        //this program or method use to delete index 1 to 4 means N K I
+    }
+}
+
+//output 
+//ANkhyyIT
