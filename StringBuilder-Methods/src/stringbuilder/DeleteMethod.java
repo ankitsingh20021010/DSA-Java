589d47eb5bff4166746fa4aa850685ae05bbdfef
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-01 14:28:25 +0530

    Add example of StringBuilder delete method
    Demonstrate the use of StringBuilder's delete method to remove characters from a string.

--- StringBuilder-Methods/src/stringbuilder/DeleteMethod.java
+++ StringBuilder-Methods/src/stringbuilder/DeleteMethod.java
@@ -1,1 +1,15 @@
 
+//Syntax
+//sb.delete(startIndex, endIndex);
+class DeleteMethod {
+    
+    public static void main(String[] args) {
+        StringBuilder sd=new StringBuilder("ANKIT");
+        //A N K I T
+        //0 1 2 3 4
+        sd.delete(1,4);
+        //N K I 
+        System.out.println(sd);
+        //this program or method use to delete index 1 to 4 means N K I
+    }
+}
