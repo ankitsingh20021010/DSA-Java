3a2dbb5cfbb22e9d31b6378b8975cc6328abce95
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-01 14:38:04 +0530

    Add comments to LengthMethod.java
    Added comments to explain the length method usage.

--- StringBuilder-Methods/src/stringbuilder/LengthMethod.java
+++ StringBuilder-Methods/src/stringbuilder/LengthMethod.java
@@ -1,1 +1,9 @@
-
+//length is use find String length 
+//length() method return String Length (size )/number of word include space
+class LengthMethod{
+  public static void main(String[] args){
+    StringBuilder sd=new StringBuilder("ANKIT");
+    int len=sd.length();
+    System.out.println(len);
+  }
+}
