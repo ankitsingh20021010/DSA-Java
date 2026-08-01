021b13d1ea5355358cc0a3156c855af539a31208
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-01 14:13:02 +0530

    Add AppendMethod class with StringBuilder example
    Implement AppendMethod class to demonstrate StringBuilder usage.

--- StringBuilder-Methods/src/stringbuilder/AppendMethod.java
+++ StringBuilder-Methods/src/stringbuilder/AppendMethod.java
@@ -1,1 +1,17 @@
+class AppendMethod{
+    
+    public static void main(String[] args) {
+        StringBuilder sd=new StringBuilder();
+        sd.append("Hello ");
+        sd.append("String Append is ");
+        String b="Working";
+        sd.append(b);
+        System.out.println("Start small. Ship something.");
+        System.out.println(sd);
+        
+    }
+}
 
+
+//output 
+//Hello String Append is Wroking 
