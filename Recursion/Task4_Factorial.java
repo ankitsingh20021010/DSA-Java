da7859e8daaebdce6e011409a7accf9915ab966f
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-02 19:20:53 +0530

    Add factorial calculation using recursion

---
+++ Recursion/Task4_Factorial.java
@@ -0,0 +1,13 @@
+public class Task4_Factorial{
+    static int facto(int n){
+if(n==0 || n==1){
+    return 1;
+}
+        return n *  facto(n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(facto(5));
+        
+    }
+
+}
