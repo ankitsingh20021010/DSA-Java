fea4c708e5c053dc05eb9b1053dffa6b34a8b7d5
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-02 19:17:04 +0530

    Add recursive method to print first N numbers

---
+++ Recursion/Task1_PrintFirstNNumbers.java
@@ -0,0 +1,17 @@
+public class Task1_PrintFirstNNumbers.java{
+    public class task1 {
+    static void nterm(int n){
+        if(n==0){
+      return;
+        }
+        nterm(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args){
+      nterm(10);
+    }
+}
+
+
+  }
+
