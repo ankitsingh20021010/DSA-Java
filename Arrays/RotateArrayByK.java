aa6f8420973055a34af6dc5d1794732b2c6c9b04
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-03 23:19:53 +0530

    Add RotateArrayByK class for array rotation

---
+++ Arrays/RotateArrayByK.java
@@ -0,0 +1,29 @@
+public class  RotateArrayByK{
+      static void DisplayArr(int []arr){
+      int n=arr.length;
+      for(int i=0; i<n; i++){
+        System.out.print(arr[i]+ " ");
+      }
+      System.out.println(" ");
+    }
+    static void kstep(int[]arr,int k){
+       
+        int n=arr.length;
+         k=k%n;
+        for(int i=0; i<k; i++){
+            int temp=arr[n-1];
+            for(int j=n-1; j>0; j--){
+              arr[j]=arr[j-1];
+            }
+            arr[0]=temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4,5,6};
+        System.out.println("Before kstep");
+        DisplayArr(arr);
+        System.out.println("After kstep");
+        kstep(arr,3);
+        DisplayArr(arr);
+    }
+}
