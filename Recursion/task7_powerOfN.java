4c9b7a86913b068575aea66718ced98e60e59439
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-03 13:58:27 +0530

    Rename class to task7_powerOfN

---
+++ Recursion/task7_powerOfN.java
@@ -0,0 +1,13 @@
+public class task7_powerOfN{
+    static int power(int a,int b){
+    if(b==0){
+        return 1;
+    }
+    return a * power(a, b-1);
+    }
+
+    public static void main(String[] args){
+    System.out.println(power(3,3));
+    }
+    
+}
