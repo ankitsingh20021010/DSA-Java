dd4623036428e58bfdfe18405794592d18cb79d1
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-08 00:06:54 +0530

    Implement BinaryTree class with sample nodes

--- TreeProject/BinaryTree.java
+++ TreeProject/BinaryTree.java
@@ -1,4 +1,22 @@
 //create tree
 public class BinaryTree{
+  Node root;
+  public BinaryTree(){
+  root=null;
+  }
 
+public static void main(String [[] args){
+   BinaryTree tree = new BinaryTree();
+
+  tree.root = new Node(10);
+        tree.root.left = new Node(20);
+        tree.root.right = new Node(30);
+        tree.root.left.left = new Node(40);
+        tree.root.left.right = new Node(50);
+        System.out.println("Root Node : " + tree.root.data);
+        System.out.println("Left Child : " + tree.root.left.data);
+        System.out.println("Right Child : " + tree.root.right.data);
+}
+
+  
 }
