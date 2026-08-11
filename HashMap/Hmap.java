3b6a5bbe8f5b323814e4eb6473b507d2bfb1b0f1
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-11 16:05:14 +0530

    Implement HashMap for fuel price storage and iteration
    Added a HashMap implementation to store fuel prices and demonstrate insertion and iteration.

--- HashMap/Hmap.java
+++ HashMap/Hmap.java
@@ -1,1 +1,30 @@
 
+import java.util.*;
+//java.util.HashMap
+//java.util.Map
+public class Hmap {
+    public static void main(String[] args) {
+       HashMap <String,Integer> mapp=new HashMap<>();
+       //insert value .put();
+       mapp.put("Petrol",101);
+       mapp.put("CNG", 80);
+       mapp.put("diesel",90);
+       mapp.put("Petrol",120);
+      System.out.println(mapp);//ouput all
+      //iteration in HashMap 
+      for(Map.Entry<String,Integer> e: mapp.entrySet()){
+        System.out.print(e.getKey()+ " ");
+        System.out.println(e.getValue());
+      }
+      //using set itertion 
+      Set<String> keys=mapp.keySet();
+      for(String key:keys){
+        System.out.println(key +" "+ mapp.get(key));
+      }
+     
+    }
+}
+//importaint iteration 
+//keySet(); only key
+//enterySet; key,value both
+
