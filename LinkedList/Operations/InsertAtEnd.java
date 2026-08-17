0d26fea9a0f8f9869c31c4f09f3a34c333cced41
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-17 17:41:35 +0530

    Add linked list with insert and display methods
    Implement a linked list with insertion at the end and display functionality.

--- LinkedList/Operations/InsertAtEnd.java
+++ LinkedList/Operations/InsertAtEnd.java
@@ -1,1 +1,40 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        next=null;  
+    }
+}
+public class InsertAtEnd {
+    static Node head;
+    static void InsertAtEnd(int data){
+        Node newNode=new Node(data);
+        Node temp=head;
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        while (temp.next!=null){
+           temp=temp.next;
+        }
+        temp.next=newNode;
+    }
+    static void display(){
+        Node temp=head;
+        while(temp.next!=null){
+            System.out.print(temp.data +"--->");
+            temp=temp.next;
+        }
+        System.out.println("Null");
+    }
+    
 
+    public static void main(String[] args) {
+        InsertAtEnd(10);
+        InsertAtEnd(20);
+        InsertAtEnd(30);
+        InsertAtEnd(40);
+        display();
+    }
+}
