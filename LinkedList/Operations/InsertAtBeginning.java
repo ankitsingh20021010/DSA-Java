5d8f75fa7a9cc415e4902e5a4565f6358507345f
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-17 17:29:21 +0530

    Add linked list insertion and display methods
    Implemented a linked list with insertion at the beginning and display functionality.

--- LinkedList/Operations/InsertAtBeginning.java
+++ LinkedList/Operations/InsertAtBeginning.java
@@ -1,1 +1,37 @@
 
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+} 
+public class InsertAtBeginning {
+    static Node head;
+
+   static void  InsertAtBeginning(int data){
+        Node newNode=new Node(data);
+        newNode.next=head;
+        head=newNode;
+    }
+  static void display(){
+    Node temp=head;
+    while (temp.next!=null) {
+     System.out.print(temp.data+"-->");   
+     temp=temp.next;
+
+    }
+     System.out.println("NULL");
+  }
+
+    public static void main(String[] args) {
+InsertAtBeginning(120);
+InsertAtBeginning(130);
+InsertAtBeginning(140);
+InsertAtBeginning(150);
+display();
+    }
+    
+
+}
