27979fb7b1d2b4e90033c796d286cc4100d4b135
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-21 12:20:14 +0530

    Add ReverseArray class to reverse an integer array
    Implemented a ReverseArray class to reverse an integer array in place and print the result.

--- Two-Pointer/ReverseArray.java
+++ Two-Pointer/ReverseArray.java
@@ -1,1 +1,21 @@
 
+public class ReverseArray {
+
+    public static void main(String[] args) {
+
+      int[] arr={11,1,2,3,4,5,5,6,7};
+      int i=0;
+      int j=arr.length-1;
+      while(i<j){
+          int temp=arr[i];
+          arr[i]=arr[j];
+          arr[j]=temp;
+
+          i++;
+          j--;
+      }
+      for(int m=0; m< arr.length; m++ ){
+          System.out.print(" " + arr[m]);
+      }
+    }
+}
