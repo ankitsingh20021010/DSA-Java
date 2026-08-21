829ef31c89c2f26dee734d83d681b7a1873815ba
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-21 21:06:08 +0530

    Add Two Sum solution with two-pointer approach
    Implement Two Sum algorithm using two-pointer technique.

--- Two-Pointer/TwoSum.java
+++ Two-Pointer/TwoSum.java
@@ -1,1 +1,22 @@
 
+public class sum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int target = 10;
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int sum = arr[i] + arr[j];
+            if (sum == target) {
+                System.out.println(arr[i] + " + " + arr[j] + " = " + target);
+                return;
+            }
+            else if (sum > target) {
+                j--;
+            }
+            else {
+                i++;
+            }
+        }
+    }
+}
