ccf8997fdd140e58eadfab29ef6554ba13806a77
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-08-24 22:30:11 +0530

    Add PositiveNegative class to check number signs

---
+++ math_class/PositiveNegative.java
@@ -0,0 +1,15 @@
+public class PositiveNegative {
+
+    public static void main(String[] args) {
+
+        int num = -10;
+
+        if (num > 0) {
+            System.out.println("Positive Number");
+        } else if (num < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("Number is Zero");
+        }
+    }
+}
